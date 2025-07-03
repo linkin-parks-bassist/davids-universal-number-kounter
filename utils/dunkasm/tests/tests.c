65c2e3eaa1cb322cf57fbbc41380fa02d69c288f
David Farrell <davidjfarrell96+2@gmail.com>
2025-07-03 15:05:40 +1000

    Made it easier to add more unit tests to tests.c

--- utils/dunkasm/tests/tests.c
+++ utils/dunkasm/tests/tests.c
@@ -4,36 +4,41 @@
 
 void test_tokenizer()
 {
-	const char *str1 = "set r0 \"test\\nstring\"";
-	char **tokens1;
+	#define N_TOKENIZER_TESTS 2
+	const char *test_strings[N_TOKENIZER_TESTS];
+	const int expected_n_tokens[N_TOKENIZER_TESTS] = {3, 3};
+	const char **expected_tokens[N_TOKENIZER_TESTS];
+	
+	test_strings[0] = strdup("set r0 \"test\\nstring\"");
+	expected_tokens[0] = malloc(3 * sizeof (char*));
+	expected_tokens[0][0] = strdup("set");
+	expected_tokens[0][1] = strdup("r0");
+	expected_tokens[0][2] = strdup("\"test\\nstring\"");
+	
+	test_strings[1] = strdup("set *(sr0+13) 0x1 %test comment");
+	expected_tokens[1] = malloc(3 * sizeof (char*));
+	expected_tokens[1][0] = strdup("set");
+	expected_tokens[1][1] = strdup("*(sr0+13)");
+	expected_tokens[1][2] = strdup("0x1");
+	
+	char **tokens;
 	int n_tokens;
 	
-	tokens1 = tokenize_string(str1, &n_tokens);
-	
-	assert(n_tokens == 3);
-	assert(strcmp(tokens1[0], "set") == 0);
-	assert(strcmp(tokens1[1], "r0") == 0);
-	assert(strcmp(tokens1[2], "\"test\\nstring\"") == 0);
-	
-	for (int i = 0; i < n_tokens; i++)
-		free(tokens1[i]);
-	
-	free(tokens1);
-	
-	const char *str2 = "set rA 0x1 %test comment";
-	char **tokens2;
-	
-	tokens2 = tokenize_string(str2, &n_tokens);
-	
-	assert(n_tokens == 3);
-	assert(strcmp(tokens2[0], "set") == 0);
-	assert(strcmp(tokens2[1], "rA")  == 0);
-	assert(strcmp(tokens2[2], "0x1") == 0);
-	
-	for (int i = 0; i < n_tokens; i++)
-		free(tokens2[i]);
-	
-	free(tokens2);
+	for (int i = 0; i < N_TOKENIZER_TESTS; i++)
+	{
+		tokens = tokenize_string(test_strings[i], &n_tokens);
+		
+		assert(n_tokens = expected_n_tokens[i]);
+		
+		for (int j = 0; j < n_tokens; j++)
+		{
+			assert(strcmp(tokens[j], expected_tokens[i][j]) == 0);
+			free(tokens[j]);
+		}
+		
+		free(tokens);
+		free(expected_tokens[i]);
+	}
 }
 
 void test_encoder()
@@ -52,10 +57,26 @@ void test_parameter_parser()
 	dasm_context cxt;
 	init_context(&cxt);
 	
-	parameter p = parse_parameter(&cxt, "argument2");
+	#define N_PP_TESTS 2
 	
-	assert(p.type = REGISTER);
-	assert(p.value = 9);
+	char *test_strings [N_PP_TESTS] = {"argument2", "*(sr0+2)"};
+	int expected_types [N_PP_TESTS] = {REGISTER, S_REGISTER | POINTER};
+	int expected_values[N_PP_TESTS] = {9, 0};
+	int expect_offset  [N_PP_TESTS] = {0, 1};
+	int expected_offset[N_PP_TESTS] = {0, 2};
+	parameter p;
+	
+	for (int i = 0; i < N_PP_TESTS; i++)
+	{
+		p = parse_parameter(&cxt, test_strings[i]);
+		
+		assert(p.type == expected_types[i]);
+		assert(p.value == expected_values[i]);
+		if (expect_offset[i])
+		{
+			assert(p.offset == expected_offset[i]);
+		}
+	}
 }
 
 int main(int argc, char **argv)
